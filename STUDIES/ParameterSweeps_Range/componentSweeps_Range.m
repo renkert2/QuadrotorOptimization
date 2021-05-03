a6fb7d79d6a3347c6f2dc2b42c731473ea83f6fb
Renkert <renkert2@illinois.edu>
2021-05-03 07:12:32 -0500

    Parameter sweeps for battery, motor, and prop

--- STUDIES/ParameterSweeps_Range/componentSweeps_Range.m
+++ STUDIES/ParameterSweeps_Range/componentSweeps_Range.m
@@ -1,15 +1,15 @@
 %% Battery
-battSweep = sweep(o, ["N_p","N_s"], 25, 'ReverseSearch', true);
-save('battSweep.mat', 'battSweep');
+battSweep = sweep(o, ["N_p","N_s"], 25, 'Objective', 'Range', 'ReverseSearch', true);
+save('battSweep_Range.mat', 'battSweep');
 title("Battery Carpet Plot")
 
 %% Motor
 constraint_func = @(Xp) distToBoundary(o.motorFit.Boundary, Xp);
-motorSweep = sweep(o, ["kV","Rm"], 25, constraint_func);
+motorSweep = sweep(o, ["kV","Rm"], 25,'Objective', 'Range', 'ConstraintFunction', constraint_func);
 
 save('motorSweep_Range.mat', 'motorSweep');
 title("Motor Carpet Plot")
 
 %% Propeller
-propSweep = sweep(o, ["D","P"], 25, 'ReverseSearch', true);
+propSweep = sweep(o, ["D","P"], 25, 'Objective', 'Range', 'ReverseSearch', true);
 save('propSweep_Range.mat', 'propSweep');
