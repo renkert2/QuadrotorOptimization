42d1f3d8d7712118bbf26bcd4a4ff6b8fbd0b579
Renkert <renkert2@illinois.edu>
2021-05-14 15:28:40 -0500

    Generalized Boundary to accept variable number of variables

--- SUPPORT_FUNCTIONS/@Boundary/Boundary.m
+++ SUPPORT_FUNCTIONS/@Boundary/Boundary.m
@@ -7,8 +7,8 @@ classdef Boundary
     %   Detailed explanation goes here
     
     properties
-        Data (:,2) double
-        BoundaryPoints (:,2) double
+        Data (:,:) double
+        BoundaryPoints (:,:) double
     end
     
     properties (SetAccess = private)
@@ -17,32 +17,70 @@ classdef Boundary
         X_mean
     end
     
+    properties (Access = private)
+        N_vars
+    end
+    
     methods
         function obj = Boundary(data)
+            % - data: each variable is a column, i.e. [X], [X Y], or [X Y Z]
+            N_vars = size(data,2);
+            obj.N_vars = N_vars;
+            
             obj.Data = data;
             obj.X_lb = min(obj.Data)';
             obj.X_ub = max(obj.Data)';
             obj.X_mean = mean(obj.Data)';
-            bound = boundary(obj.Data(:,1), obj.Data(:,2),0);
-            obj.BoundaryPoints = obj.Data(bound,:);
+            
+            bound_flag = false;
+            switch N_vars
+                case 2
+                    bound = boundary(obj.Data(:,1), obj.Data(:,2), 0);
+                    bound_flag = true;
+                case 3
+                    bound = boundary(obj.Data(:,1), obj.Data(:,2), obj.Data(:,3), 0);
+                    bound_flag = true;
+            end
+            
+            if bound_flag
+                obj.BoundaryPoints = obj.Data(bound,:);
+            end
         end
         
         function d = distToBoundary(obj,varargin)
-            if nargin == 2
-                X = varargin{1};
-                X1 = X(1,:);
-                X2 = X(2,:);
-            elseif nargin == 3
-                X1 = varargin{1};
-                X2 = varargin{2};
+            switch obj.N_Vars
+                case 1
+                    X = varargin{1};
+                    in_bounds = (obj.X_lb <= X) && (X <= obj.X_ub);
+                    
+                    d = min(abs(X - [obj.X_lb obj.X_ub]));
+                    if in_bounds
+                        d = -d;
+                    end
+                case 2
+                    if nargin == 2
+                        X = varargin{1};
+                        X1 = X(1,:);
+                        X2 = X(2,:);
+                    elseif nargin == 3
+                        X1 = varargin{1};
+                        X2 = varargin{2};
+                    end
+                    
+                    d = obj.p_poly_dist(X1,X2,obj.BoundaryPoints(:,1), obj.BoundaryPoints(:,2),true)';
+                otherwise
+                    d = NaN;
             end
-            
-            d = obj.p_poly_dist(X1,X2,obj.BoundaryPoints(:,1), obj.BoundaryPoints(:,2),true)';
         end
         
         function l = isInBoundary(obj,varargin)
-            d = distToBoundary(obj,varargin{:});
-            l = (d <= 0);
+            switch obj.N_vars
+                case 1
+                    l = (obj.X_lb <= X) && (X <= obj.X_ub);
+                case 2
+                    d = distToBoundary(obj,varargin{:});
+                    l = (d <= 0);
+            end
         end
         
         function [x1_vals, x2_vals] = createGrid(obj, res)
