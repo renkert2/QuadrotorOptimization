e09cb641134afc69676d2573427a07033d727d0e
Renkert <renkert2@illinois.edu>
2021-10-22 14:05:13 -0500

    Changes to Body Model

--- COMPONENTS/BodyModel.m
+++ COMPONENTS/BodyModel.m
@@ -67,11 +67,16 @@ classdef BodyModel < Model
                 obj.Params(i).Tunable = true;
             end
             obj.setSymVars();
-            setModelSymFunctions(obj)
+            setModelSymFunctions(obj, "IncludeGyroTorques", false)
             init@Model(obj);
         end
         
-        function setModelSymFunctions(obj)
+        function setModelSymFunctions(obj, opts)
+            arguments
+                obj
+                opts.IncludeGyroTorques = true;
+            end
+            
             x = obj.SymVars.x;
             d = obj.SymVars.d;
             p = x(obj.I.x.p);
@@ -103,7 +108,11 @@ classdef BodyModel < Model
             
             [f,tau] = obj.controlEffectiveness(omega_tilde, K_T, K_Q, l);
 
-            G_a = J_r*([-1 1 -1 1]*omega_tilde)*(cross(e_3, omega));
+            if opts.IncludeGyroTorques
+                G_a = J_r*([-1 1 -1 1]*omega_tilde)*(cross(e_3, omega));
+            else
+                G_a = zeros(3,1);
+            end
 
             obj.f_sym = [v;...
                 g*e_3 + (1/m)*Rbe*(-f*e_3) + d./m;...
@@ -112,14 +121,88 @@ classdef BodyModel < Model
             
             obj.g_sym = x;
         end
-        
+
+        function [t, omega_tilde_des, x_des, x_des_0]  = InverseBodyModel(obj, rt, opts)
+            % Takes desired reference trajectory and outputs required rotor
+            % speeds.  a_des is a 3xN matrix, where N is the number of sample
+            % points.
+            arguments
+                obj
+                rt ReferenceTrajectory3D
+                opts.SetInitialConditions logical = true
+            end
+            
+            t = rt.t;
+            dT = rt.delta_T;
+            p_des = rt.R_Data;
+            v_des = rt.dR_Data;
+            a_des = rt.ddR_Data;
+                        
+            N = numel(t);
+
+            % Start with single point for a_des; convert to vectorized
+            % later
+
+            % Parameter Values: J,m,J_r,K_T,K_Q,l
+            J = obj.J.Value;
+            m = obj.m.Value;
+            J_r = obj.J_r.Value;
+            K_T = obj.K_T.Value;
+            K_Q = obj.K_Q.Value;
+            l = obj.l.Value;
+            g = obj.g;
+            e_3 = [0;0;1];
+
+            % 1) Calculate Desired Force Vector
+            e_f_des = m*(a_des - g*repmat(e_3, [1 N]));
+
+            f_des = vecnorm(e_f_des, 2);
+            e_z_b_des = -e_f_des./repmat(f_des, [3 1]);
+            
+            % 2) Calculate Desired Orientation
+            theta = -asin(e_z_b_des(1,:));
+            phi = asin(e_z_b_des(2,:)./cos(theta));
+            psi = zeros(1,N);
+
+            Theta_des = [phi; theta; psi];
+
+            % 3) Calculate Desired angular velocities and accelerations
+            Theta_dot = diff(Theta_des,1,2)/dT;
+            Theta_dot = [Theta_dot, Theta_dot(:,end)];
+            
+            b_omega_des = zeros(3,N);
+            for i = 1:N
+                b_omega_des(:,i) = BodyModel.WInv(phi(i), theta(i))*Theta_dot(:,i);
+            end
+            b_omega_des_dot = diff(b_omega_des, 1, 2)/dT;
+            b_omega_des_dot = [b_omega_des_dot, b_omega_des_dot(:,end)];
+
+            % 4) Calculate Desired Forces and Moments + Rotor Speeds,
+            % Neglect Gyroscopic Torque for now
+            tau_des = J*b_omega_des_dot + cross(b_omega_des, J*b_omega_des, 1);
+
+            C = BodyModel.controlEffectivenessMatrix(K_T, K_Q, l);
+            omega_tilde_des = sqrt(C\[f_des; tau_des]);
+            
+            x_des = zeros(obj.Nx,N);
+            x_des(obj.I.x.p, :) = p_des;
+            x_des(obj.I.x.v, :) = v_des;
+            x_des(obj.I.x.Theta, :) = Theta_des;
+            x_des(obj.I.x.omega, :) = b_omega_des;
+            
+            x_des_0 = x_des(:,1);
+            if opts.SetInitialConditions
+                obj.x0 = x_des_0;
+            end
+        end
+
         function [ax] = plot(obj, x_arg, opts)
             arguments
                 obj
                 x_arg
                 opts.Interval double = 10
                 opts.ParentAxes matlab.graphics.axis.Axes = matlab.graphics.axis.Axes.empty()
-                opts.RefTraj timeseries = timeseries.empty()
+                opts.RefTraj = ReferenceTrajectory3D.empty()
                 opts.Annotate logical = true
                 opts.TrajectoryNames string = string.empty()
                 opts.Painter logical = true
@@ -135,7 +218,14 @@ classdef BodyModel < Model
             
             if ~isempty(opts.RefTraj)
                 ln_ref_flag = true;
-                x = opts.RefTraj.Data;
+                if isa(opts.RefTraj, 'ReferenceTrajectory3D')
+                    ts = opts.RefTraj.TimeSeries;
+                elseif isa(opts.RefTraj, 'timeseries')
+                    ts = opts.RefTraj;
+                else
+                    error('RefTraj must be a ReferenceTrajectory3D or a timeseries');
+                end
+                x = ts.Data;
                 ln_ref = plot3(ax, x(:,1), -x(:,2), -x(:,3), '-b');
             else
                 ln_ref_flag = false;
@@ -304,6 +394,12 @@ classdef BodyModel < Model
                 0 cos(phi) -sin(phi);...
                 0 sin(phi)/cos(theta) cos(phi)/cos(theta)];
         end
+
+        function b_omega = WInv(phi, theta)
+            b_omega = [1 0 -sin(theta);...
+                0 cos(phi) cos(theta)*sin(phi);...
+                0 -sin(phi) cos(theta)*cos(phi)];
+        end
         
         function rz = Rz(psi)
             rz = [cos(psi) sin(psi) 0;...
@@ -326,12 +422,16 @@ classdef BodyModel < Model
         function rbe = Rbe(phi,theta,psi)
             rbe = BodyModel.Rx(phi)*BodyModel.Ry(theta)*BodyModel.Rz(psi);
         end
-        
-        function [f,tau] = controlEffectiveness(omega_tilde, K_T, K_Q, l)
-            x = [K_T K_T K_T K_T;...
+
+        function C = controlEffectivenessMatrix(K_T, K_Q, l)
+            C = [K_T K_T K_T K_T;...
                 l*K_T -l*K_T -l*K_T l*K_T;...
                 l*K_T l*K_T -l*K_T -l*K_T;...
-                K_Q -K_Q K_Q -K_Q]*omega_tilde.^2;
+                K_Q -K_Q K_Q -K_Q];
+        end
+        
+        function [f,tau] = controlEffectiveness(omega_tilde, K_T, K_Q, l)
+            x = BodyModel.controlEffectivenessMatrix(K_T,K_Q,l)*omega_tilde.^2;
             f = x(1);
             tau = x(2:4);
         end
