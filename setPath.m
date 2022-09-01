d1c2e1cdb627b456986fcf2b721374bbbb1d0525
Renkert <renkert2@illinois.edu>
2022-09-01 10:25:36 -0500

    Added GraphTools submodule in setPath

--- setPath.m
+++ setPath.m
@@ -1,3 +1,7 @@
+addpath(genpath('GraphTools_Phil_V1/GRAPH_CLASS'));
+addpath(genpath('GraphTools_Phil_V1/SUPPORT_FILES'));
+addpath(genpath('GraphTools_Phil_V1/SUPPORT_FUNCTIONS'));
+
 addpath(genpath('COMPONENTS'));
 addpath('STUDIES');
 addpath('TESTS');
